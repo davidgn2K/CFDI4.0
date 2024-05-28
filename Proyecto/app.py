3dd55e653dff36d41be301fe7676ef5550abf7c0
davidgn2k <davidgn2k@gmail.com>
2024-05-28 16:19:39 -0600

    Index visual revamp.

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -5,5 +5,6 @@ app =  Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == '__main__':
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 10
     app.run(debug=True, port=8000)
 
